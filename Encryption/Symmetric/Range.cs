30273b06077d2f03f9c6ff597aa5d81c1ce91c01
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-13 17:29:35 +0330

    Create Range.cs

---
+++ Encryption/Symmetric/Range.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Threading;
+
+namespace Cactus.Blade.Encryption.Symmetric
+{
+    internal class Range
+    {
+        private static readonly ThreadLocal<RandomNumberGenerator> Instance =
+            new ThreadLocal<RandomNumberGenerator>(RandomNumberGenerator.Create);
+
+        public static byte[] GetBytes(int size)
+        {
+            var data = new byte[size];
+
+            Instance.Value.GetBytes(data);
+
+            return data;
+        }
+    }
+}
