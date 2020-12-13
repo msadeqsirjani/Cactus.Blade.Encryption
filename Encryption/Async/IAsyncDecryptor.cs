6200365f6c58833f53c0b5a91785d6625355e795
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-13 13:56:06 +0330

    Create IAsyncDecryptor.cs

---
+++ Encryption/Async/IAsyncDecryptor.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cactus.Blade.Encryption.Async
+{
+    /// <summary>
+    /// Defines methods for asynchronous decryption.
+    /// </summary>
+    public interface IAsyncDecryptor
+    {
+        /// <summary>
+        /// Asynchronously decrypts the specified cipher text.
+        /// </summary>
+        /// <param name="cipherText">The cipher text.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A task whose result represents the decrypted value as a string.</returns>
+        Task<string> DecryptAsync(string cipherText, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously decrypts the specified cipher text.
+        /// </summary>
+        /// <param name="cipherText">The cipher text.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A task whose result represents the decrypted value as a byte array.</returns>
+        Task<byte[]> DecryptAsync(byte[] cipherText, CancellationToken cancellationToken = default);
+    }
+}
